9cb9e32f8f1a487d7b4d014f47a1e33b5646fa21
zhouwei <229739502@qq.com>
2024-09-18 17:18:10 +0800

    全局禁用鼠标中键返回

--- app/src/main/java/com/limelight/HelpActivity.java
+++ app/src/main/java/com/limelight/HelpActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.graphics.Bitmap;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.window.OnBackInvokedCallback;
@@ -115,15 +114,4 @@ public class HelpActivity extends Activity {
         }
     }
 
-    @Override
-    public boolean onMouseEvent(MotionEvent event) {
-        if (event.getToolType(0) == MotionEvent.TOOL_TYPE_MOUSE) {
-            int buttonState = event.getButtonState();
-            if ((buttonState & MotionEvent.BUTTON_TERTIARY) != 0) {
-                // 处理鼠标中键点击事件
-                return true;
-            }
-        }
-        return super.onMouseEvent(event);
-    }
 }
